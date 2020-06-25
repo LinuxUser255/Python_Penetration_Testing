e45e80e4ddb28c00374252cb618d0d3a6ba4bf08
Chris <atnl@protonmail.com>
2020-06-25 16:11:35 +0000

    Add files via upload

---
+++ forloop1.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+
+# i becomes each variable 
+for i in [1,2,3,4,5,'a','b','c']:
+    print(i)
+    
+print('------------------------')
+
+for i in range(26):
+    print("{}".format(i))
+
+print('------------------------')
+
+for i in range(0, 26):    
+    print(i)
+
