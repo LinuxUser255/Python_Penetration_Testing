eb5f31e793a70860d19bbb2731b0269f6d5d9e11
Chris <atnl@protonmail.com>
2020-06-26 20:21:03 +0000

    Update forloop1.py

--- forloop1.py
+++ forloop1.py
@@ -1,16 +1,22 @@
 #!/usr/bin/env python3
 
-# i becomes each variable 
+#The for loop takes a range of values and asssigns them one-by-one to a variable.
+# i becomes each variable. Four examples.
 for i in [1,2,3,4,5,'a','b','c']:
     print(i)
-    
-print('------------------------')
+for c in 'password':
+    print(c)
 
+#Use of the replacement field {}. to format and print i. 
 for i in range(26):
     print("{}".format(i))
 
-print('------------------------')
 
+for i in range(1,25):
+     print("i is now {}".format(i))
+
+
+#This loop retrieves the values of 0 through 26.
+#The last value is not included and thus ends on 25.
 for i in range(0, 26):    
     print(i)
-
