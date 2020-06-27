63251e8b51f2bba837cebe43ec090e48217c9a2f
Chris <atnl@protonmail.com>
2020-06-27 04:07:08 +0000

    Update backdoor.py

--- Python_Networking/backdoor.py
+++ Python_Networking/backdoor.py
@@ -1,9 +1,16 @@
 #!/usr/bin/env python3
 
+#Backdoor --- (Rough draft)
 import socket
 import os
 import platform
+import glob, re
 
 
-#Backdoor
-# Get sysinfo & the content of a remote folder
+#Get sys info & the contents of a remote folder
+for msg in glob.glob('/tmp/'.txt'):
+        filer = open((msg),'r')
+        data = filer.read()
+        message = re.findall(r' message (. ?) /message ', data,re.DOTALL)
+        print "File %s conmtains %s % (str(msg),message)
+        filer.close()
