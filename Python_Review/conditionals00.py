4a18a79e0fd3543ec7c15fa23fc5f092f92a9f22
Chris <atnl@protonmail.com>
2020-06-30 16:41:45 +0000

    Add files via upload

---
+++ Python_Review/conditionals00.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+
+
+# Conditionals first example. 
+x = 55
+
+#if x < 550:
+#    print("x is smaller than 55")
+    
+
+#if x < 40 or x > 50:
+#    print("x is under 40")
+#else:
+#    print("x is above 50")
+
+if x < 40 or x > 50:
+    print("x is under 40")
+else:
+    print("x is above 50")
+if x == 55:
+    print("x is 55")
+else:
+    print("x is not 55")
+
+
