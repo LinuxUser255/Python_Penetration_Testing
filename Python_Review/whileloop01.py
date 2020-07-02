4b8d27e4861f800bfc4de1a55986bea65c007edb
Chris <atnl@protonmail.com>
2020-07-02 03:05:02 +0000

    Update and rename whileloop2.py to whileloop01.py

---
+++ Python_Review/whileloop01.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+
+import time
+
+# While loop example
+# while a condition is true, do..
+while (25 + 25 == 50):
+        time.sleep(0.1)
+        print('continue looping')
+
