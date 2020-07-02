3bbf642e8e9686068180df8ba2f628741b2fd51d
Chris <atnl@protonmail.com>
2020-07-02 02:56:08 +0000

    Update forloop1.py

--- Python_Review/forloop1.py
+++ Python_Review/forloop1.py
@@ -1,7 +1,14 @@
 #!/usr/bin/env python3
 
-#The for loop takes a range of values and asssigns them one-by-one to a variable.
+# The For Loop
+
 # i becomes each variable. Four examples.
+#This loop retrieves the values of 0 through 26.
+#The last value is not included and thus ends on 25.
+for i in range(0, 26):    
+    print(i)
+
+# Iterate through a range
 for i in [1,2,3,4,5,'a','b','c']:
     print(i)
 for c in 'password':
@@ -14,13 +21,5 @@ for i in range(26):
 
 for i in range(1,25):
      print("i is now {}".format(i))
-
-
-#This loop retrieves the values of 0 through 26.
-#The last value is not included and thus ends on 25.
-for i in range(0, 26):    
-    print(i)
-
-
 
 
