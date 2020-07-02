264491cc4d783d44f71e5683b3bfedcac2f93e35
Chris <atnl@protonmail.com>
2020-07-02 02:52:28 +0000

    Create  conditionals01.py

---
+++ Python_Review/conditionals01.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+
+#Conditions: if else elif.
+#A few ways to write them.
+
+#Reading user input 
+#type = input("Scan type? TCP or UDP: ")
+#port = int(type("Enter the port number: {0}, ")
+       # print("Scanning port {0}, ".format(type))
+
+#int = int(input("Enter port number to scan: {0}, ".format(type)))
+
+#Conditional
+#Using () or () 
+if (age < 16) or (age > 90):
+    print("It's best to call a cab.") 
+else:
+    print("You can drive.")
+
+
+#When "and" is used as such: ( ) and  ( ) both statements must be true to satisfy the condition.
+#However, when using "or" ( ) or ( ) then only one of the two statements 
+#must be true, and must be follwed with an else
