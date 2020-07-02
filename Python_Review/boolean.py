77c3895f94e765c706ae50430ab35c0c679be0eb
Chris <atnl@protonmail.com>
2020-07-02 02:44:42 +0000

    Update boolean.py

--- Python_Review/boolean.py
+++ Python_Review/boolean.py
@@ -1,25 +1,20 @@
 #!/usr/bin/env python3
 
-# I may, or may not use this one. But posting it none the-less
+#Truth Value Testing
+# constants defined to be false: None and False.
+# zero of any numeric type: 0, 0.0, 0j, Decimal(0), Fraction(0, 1)
+# empty sequences and collections: '', (), [], {}, set(), range(0)
 
-#Python does not have boolean data type, but it does have two constants: TRUE and FALSE
-#Example: using x as a string. In Python, True is 1 and False is 0.
-#But in a condition, any non-zero or non empty values will evaluate to true.
 
-x = "false"
-if x:
-    print("x is true")
-print("""False: {0} 
-None: {1}
-0: {2}
-0.0: {3}
-empty list []: {4}
-empty tuple (): {5}
-empty string ' ': {6}
-empty string "": {7}
-empty mapping {{}}: {8}
-""".format(False,bool(None), bool(0), bool(0.0), bool([]), bool(()), bool(' '), bool(""), bool({})))
+if 0:
+    print("True")
+else:
+    print("False")
 
-#NOT TRUE = FALSE & FALSE = NOT TRUE
-print(not False)
-print(not True)
+OS = input("What Operating System are you using? ")
+if OS != "":
+     print("How long you been runnin' " + str(OS) + "?")
+     
+else:
+    print("What? You dont use computers?")
+
