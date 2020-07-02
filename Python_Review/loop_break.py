fbe3094be0097504c340a58b1ea578efe2094fd4
Chris <atnl@protonmail.com>
2020-07-02 03:08:09 +0000

    Create loop_break.py

---
+++ Python_Review/loop_break.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+
+#demonstration of break and continue
+var = 40
+
+while var > 0:              
+   print("var > 0")
+   var = var -1
+   if var == 20:
+      break
+      #continue
+
+("loop break")
+#print("loop completed")
