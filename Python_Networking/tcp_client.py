d19f7bd37a6e926e001c8cf8464a564c3991317a
Chris <atnl@protonmail.com>
2020-07-07 15:04:25 +0000

    Add files via upload

---
+++ Python_Networking/tcp_client.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+
+import socket
+
+target_host = "google.com"
+target_port = 80
+
+#creating a socket object
+client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+#connnecting the client
+client connect((target_host, target_port))
+
+#sending data
+client.send("GET/HTTP.1.1\r\nHost: google.com\r\n\r\n")
+
+#receiving data
+response = client.recv(4096)
+
+
+print response
