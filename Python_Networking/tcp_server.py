f90b6e3ef9bca394d74a4f200a77ab6fe6a9d747
Chris <atnl@protonmail.com>
2020-07-11 01:37:27 +0000

    Update tcp_server.py

--- Python_Networking/tcp_server.py
+++ Python_Networking/tcp_server.py
@@ -1,33 +1,30 @@
 #!/usr/bin/env python
 
-import socket
+# TCP SERVER
+import  socket
 import threading
 
-bind_ip "0.0.0.0"
+bind_ip = "0.0.0.0"
 bind_port = 9999
 
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 server.bind((bind_ip,bind_port))
 server.listen(5)
+print("[*] Listening on  %s:%d" % (bind_ip,bind_port))
 
-#server loops awaiting incoming connection
-print "[*] Listening on %s:%d" % (bind_ip,bind_port)
-
+#Client handling thread Function
 def handle_client(client_socket):
-    
-    #Print what the client sends
     request = client_socket.recv(1024)
-    print "[*] Received: %s" % request
-
-    #return a packet
+    print("[*] Received: %s" % request)
     client_socket.send("ACK!")
     client_socket.close()
 
 while True:
-    client,addr = server.accept()
-    print"[*] Accepted connection from: %s:%d" % (addr[0],addr[1])
 
-#create client thread to handle incoming data
-client_handler = threading.Thread.Thread(target=handle_client,args=(client,))
-cliient_handler.start()
+    client,addr = server.accept()
+    print("[*] Accepted connection from: %s:%d" % (addr[0],addr[1]))
+
+    #Calling the Function:
+    client_handler = threading.Thread(target=handle_client,args=(client,))
+    client_handler.start()
 
