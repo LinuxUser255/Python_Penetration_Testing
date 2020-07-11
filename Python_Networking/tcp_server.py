f5e44e11cbbf702bdc738679118414e484c80655
Chris <atnl@protonmail.com>
2020-07-11 01:40:17 +0000

    Update tcp_server.py

--- Python_Networking/tcp_server.py
+++ Python_Networking/tcp_server.py
@@ -12,10 +12,12 @@ server.bind((bind_ip,bind_port))
 server.listen(5)
 print("[*] Listening on  %s:%d" % (bind_ip,bind_port))
 
-#Client handling thread Function
+# Client handling-thread Function
 def handle_client(client_socket):
+    # Print what the client sends
     request = client_socket.recv(1024)
     print("[*] Received: %s" % request)
+    # Return a packet
     client_socket.send("ACK!")
     client_socket.close()
 
