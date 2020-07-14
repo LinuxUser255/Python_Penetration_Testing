37b9a9f576f41e3d7e082cb835c571ac9f880b60
Chris <atnl@protonmail.com>
2020-07-14 22:40:34 +0000

    Add files via upload

---
+++ Python_Review/functions01.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+
+# First you must define the function
+def pro_lang(language):
+  print(f"We are learning {language.title()}.")
+
+#Then call the function
+pro_lang('Python functions')
+
