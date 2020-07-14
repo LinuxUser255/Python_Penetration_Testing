3d0291bdf245346ab134e60d700435fec64ab9d2
Chris <atnl@protonmail.com>
2020-07-14 22:44:20 +0000

    Add files via upload

---
+++ Python_Review/var_decide.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+
+#User defined variables. Asking for input.
+age = int(input("your age? "))
+
+if (age < 16) or (age > 90):
+    print("It's best to call a cab.") 
+else:
+    print("You can  drive.")
+
---
+++ Python_Review/work.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+
+#Conditional if else
+age = int(input("your age? ")) 
+
+if  age >= 16:
+    print("Yes, you are old enough to work.")
+else:
+    print("No, you must wait untill you are 16.")
+
+
+
+
+
+
