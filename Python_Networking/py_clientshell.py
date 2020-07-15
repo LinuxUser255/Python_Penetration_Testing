f0bf60d6724aa7f4ad0f16524ca30ebad04626a9
Chris <atnl@protonmail.com>
2020-07-15 17:30:47 -0400

    Update py_clientshell.py

--- Python_Networking/py_clientshell.py
+++ Python_Networking/py_clientshell.py
@@ -9,16 +9,10 @@ SERVER_HOST = "192.168.1.10"
 SERVER_PORT = 4444
 BUFFER_SIZE = 1024
 
-
 # create the socket object
 s = socket.socket()
 # connect to the server
 s.connect((SERVER_HOST, SERVER_PORT))
-
-
-# receive the greeting message
-message = s.recv(BUFFER_SIZE).decode()
-print("Server:", message)
 
 while True:
     # receive the command from the server
