cb563f7487fd56cef9127b3ab0e0b7ed4bae065f
Chris <atnl@protonmail.com>
2021-04-16 16:11:03 -0400

    Update whileloop000.py

--- Python_Review/whileloop000.py
+++ Python_Review/whileloop000.py
@@ -2,7 +2,23 @@
 
 import time
 
-# Exmple 1
+# While Loop
+i = 1
+
+while i > 0:
+    time.sleep(0.1)
+    i += 1
+    print(i)
+    if i == 20:
+        break
+
+("loop break")
+
+#------------------------------------------------------
+print("\n")
+#-----------------------------------------------------
+
+
 num = 1
 while num <= 5:
     print(num)
